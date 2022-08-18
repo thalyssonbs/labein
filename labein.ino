e7a6b38b0ed0c814836b1de53f4b0da1c0a29b5f
Thalysson Santos <thalysson.santos@icloud.com>
2022-08-17 23:15:34 -0400

    create secrets file

--- labein.ino
+++ labein.ino
@@ -11,17 +11,9 @@ const uint32_t connectTimeoutMs = 10000;
 #include "ThingSpeak.h"
 #include "Secrets.h"
 
-unsigned long myChannelNumber = 1826839;
-const char * myWriteAPIKey = "0SI57X1WA9TRN8QD";
 
 
-#define WIFI_SSID         "Thalysson"
-#define WIFI_PASS         "hjpj64y8"
-#define APP_KEY           "53a899e4-8918-44dc-b228-6482008e0228"      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
-#define APP_SECRET        "87b78883-a4ec-4a09-8b99-154aaee1f32b-2afcc2d3-0506-4726-b16e-6d1b46bffea6"   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
-#define TEMP_SENSOR_ID    "62b372d50aec232058ee3189"    // Should look like "5dc1564130xxxxxxxxxxxxxx"
-#define DEVICE_ID         "62c1f5200aec232058f4ecfc"
-#define UMIDIF_ID         "62b38bae0aec232058ee4054"
+
 #define BAUD_RATE         9600  // Not using serial!
 #define EVENT_WAIT_TIME   30000              
 
